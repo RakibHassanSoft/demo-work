8583adf4875506e388f2ebd13d7d71d1a2abff4f
RakibHassanSoft <rakibreings32@gmail.com>
2025-09-16 10:56:28 +0600

    Navbar is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 
 import './App.css'
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div>
+      <div className='w-7xl mx-auto '>
+
+      <Navbar/>
+      </div>
       THis is home page
     </div>
   );
